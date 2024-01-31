4910a937e2c721a2c8a4794b60f06dbdead73872
DimensionSTP <ddang8jh@gmail.com>
2024-01-31 14:30:08 +0900

    update: update log and save name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pytorch_lightning.utilities.distributed import rank_zero_info
 from src.pipelines.pipeline import train, test, tune
 
 
-@hydra.main(config_path="configs/", config_name="multimodal_main.yaml")
+@hydra.main(config_path="configs/", config_name="multimodal.yaml")
 def main(config: DictConfig,) -> None:
     if config.is_tuned:
         params = json.load(open(config.tuned_hparams_path, "rt", encoding="UTF-8"))
