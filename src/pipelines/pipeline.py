137f87ae8e3b9f2d8b3d0220c995681f75cf1c6a
DimensionSTP <ddang8jh@gmail.com>
2024-02-18 02:11:01 +0900

    update: resumed training, deepspeed_stage_3 test

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -1,7 +1,12 @@
+import os
+
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
 from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.utilities.deepspeed import (
+    convert_zero_checkpoint_to_fp32_state_dict,
+)
 
 from ..utils.setup import SetUp
 from ..tuners.multimodal_tuner import MultiModalTuner
@@ -38,15 +43,33 @@ def train(
     logger.log_hyperparams(logged_hparams)
 
     trainer: Trainer = instantiate(
-        config.trainer, callbacks=callbacks, logger=logger, _convert_="partial"
+        config.trainer,
+        callbacks=callbacks,
+        logger=logger,
+        _convert_="partial",
     )
 
     try:
-        trainer.fit(
-            model=architecture,
-            train_dataloaders=train_loader,
-            val_dataloaders=val_loader,
-        )
+        if isinstance(config.resumed_step, int):
+            if config.resumed_step == 0:
+                trainer.fit(
+                    model=architecture,
+                    train_dataloaders=train_loader,
+                    val_dataloaders=val_loader,
+                )
+            elif config.resumed_step > 0:
+                trainer.fit(
+                    model=architecture,
+                    train_dataloaders=train_loader,
+                    val_dataloaders=val_loader,
+                    ckpt_path=f"{config.callbacks.model_checkpoint.dirpath}/last.ckpt",
+                )
+            else:
+                raise ValueError(
+                    f"Invalid resumed_step argument: {config.resumed_step}"
+                )
+        else:
+            raise TypeError(f"Invalid resumed_step argument: {config.resumed_step}")
         logger.experiment.alert(
             title="Training Complete",
             text="Training process has successfully finished.",
@@ -59,6 +82,18 @@ def train(
             level="ERROR",
         )
         raise e
+
+    if (
+        config.strategy == "deepspeed_stage_3"
+        or config.strategy == "deepspeed_stage_3_offload"
+    ):
+        for epoch in range(config.epoch):
+            ckpt_path = f"{config.callbacks.model_checkpoint.dirpath}/epoch{epoch}.ckpt"
+            if os.path.exists(ckpt_path) and os.path.isdir(ckpt_path):
+                convert_zero_checkpoint_to_fp32_state_dict(
+                    ckpt_path,
+                    f"{ckpt_path}/model.pt",
+                )
 
 
 def test(
@@ -90,16 +125,41 @@ def test(
             logged_hparams[key] = value
     logger.log_hyperparams(logged_hparams)
 
-    trainer: Trainer = instantiate(
-        config.trainer, callbacks=callbacks, logger=logger, _convert_="partial"
-    )
+    if (
+        config.strategy == "deepspeed_stage_3"
+        or config.strategy == "deepspeed_stage_3_offload"
+    ):
+        trainer: Trainer = instantiate(
+            config.trainer,
+            strategy="ddp",
+            callbacks=callbacks,
+            logger=logger,
+            _convert_="partial",
+        )
+    else:
+        trainer: Trainer = instantiate(
+            config.trainer,
+            callbacks=callbacks,
+            logger=logger,
+            _convert_="partial",
+        )
 
     try:
-        trainer.test(
-            model=architecture,
-            dataloaders=test_loader,
-            ckpt_path=config.ckpt_path,
-        )
+        if (
+            config.strategy == "deepspeed_stage_3"
+            or config.strategy == "deepspeed_stage_3_offload"
+        ):
+            trainer.test(
+                model=architecture,
+                dataloaders=test_loader,
+                ckpt_path=f"{config.ckpt_path}/model.pt",
+            )
+        else:
+            trainer.test(
+                model=architecture,
+                dataloaders=test_loader,
+                ckpt_path=config.ckpt_path,
+            )
         logger.experiment.alert(
             title="Testing Complete",
             text="Testing process has successfully finished.",
@@ -127,6 +187,9 @@ def tune(
     logger = setup.get_wandb_logger()
 
     tuner: MultiModalTuner = instantiate(
-        config.tuner, train_loader=train_loader, val_loader=val_loader, logger=logger
+        config.tuner,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        logger=logger,
     )
     tuner()
