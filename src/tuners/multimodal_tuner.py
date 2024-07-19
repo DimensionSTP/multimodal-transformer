42a22939b8146c3a8f0b3a041d91408e36f1b893
DimensionSTP <ddang8jh@gmail.com>
2024-07-20 06:10:17 +0900

    feat: weight decay hparams tuning

--- src/tuners/multimodal_tuner.py
+++ src/tuners/multimodal_tuner.py
@@ -137,6 +137,13 @@ class MultiModalTuner:
                 high=self.hparams.lr.high,
                 log=self.hparams.lr.log,
             )
+        if self.hparams.weight_decay:
+            params["weight_decay"] = trial.suggest_float(
+                name="weight_decay",
+                low=self.hparams.weight_decay.low,
+                high=self.hparams.weight_decay.high,
+                log=self.hparams.weight_decay.log,
+            )
         if self.hparams.period:
             params["period"] = trial.suggest_int(
                 name="period",
@@ -174,6 +181,7 @@ class MultiModalTuner:
             average=self.module_params.average,
             strategy=self.module_params.strategy,
             lr=params["lr"],
+            weight_decay=params["weight_decay"],
             period=params["period"],
             eta_min=params["eta_min"],
             interval=self.module_params.interval,
