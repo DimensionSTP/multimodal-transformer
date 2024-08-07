b0b551dd144383f4eb0614cb86c8870ebb23f1ff
DimensionSTP <ddang8jh@gmail.com>
2024-08-07 13:48:09 +0900

    feat: model_detail, merge_model

--- scripts/upload_to_hf_hub.sh
+++ scripts/upload_to_hf_hub.sh
@@ -6,10 +6,12 @@ strategy="ddp"
 precision=32
 batch_size=64
 epoch=10
+model_detail="multimodal-transformer"
 
 python $path/upload_to_hf_hub.py \
     is_tuned=$is_tuned \
     strategy=$strategy \
     precision=$precision \
     batch_size=$batch_size \
-    epoch=$epoch
+    epoch=$epoch \
+    model_detail=$model_detail
